dd27f646e9e69b74ef5f27acca29949a277bdecf
Todd Boyce Jr <tjboyce@me.com>
2019-03-08 16:46:46 -0600

    set up server side code

--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
+const portfolio = require('./routes/portfolio.router');
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/portfolio', portfolio);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
